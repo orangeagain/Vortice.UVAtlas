a45ff8e4405abe6598caa0680fbfbc636c06c67b
Amer Koleci <amerkoleci@gmail.com>
2021-04-05 12:08:23 +0200

    Initial bindings project and test project.

---
+++ src/Vortice.UVAtlas.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vortice.UVAtlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
