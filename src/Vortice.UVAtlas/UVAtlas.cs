766681a65f892b3957b4c240f1468d635cad97ae
Amer Koleci <amerkoleci@gmail.com>
2021-04-11 10:18:52 +0200

    Add NuGet.Config to root.

---
+++ src/Vortice.UVAtlas/UVAtlas.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Amer Koleci and contributors.
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vortice.UVAtlas
+{
+    public static class UVAtlas
+    {
+    }
+}
